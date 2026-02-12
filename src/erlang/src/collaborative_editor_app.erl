b0b5791dcd4b9edb4d1e59f2a0148f967e413af6
yughias <sasa.laporta@gmail.com>
2026-02-12 21:58:02 +0100

    added sync of mnesia on different nodes

--- src/erlang/src/collaborative_editor_app.erl
+++ src/erlang/src/collaborative_editor_app.erl
@@ -27,23 +27,30 @@ stop(_State) ->
 
 init_mnesia() ->
     %% 1. Ensure the Schema itself is stored on disk.
-    %%    Since Mnesia is already running (ram_copies by default on first run),
-    %%    we change the schema table to disc_copies. This persists the DB.
     case mnesia:change_table_copy_type(schema, node(), disc_copies) of
         {atomic, ok} -> ok;
-        {aborted, {already_exists, schema, _Node, _Type}} -> ok; %% Already on disk
+        {aborted, {already_exists, schema, _Node, _Type}} -> ok;
         {aborted, Reason} -> error(Reason)
     end,
 
-    %% 2. Create the table (if it doesn't exist)
+    %% 2. Create the table OR add a local copy if it exists remotely
     case mnesia:create_table(editor_docs, [
         {attributes, [doc_id, content]}, 
         {disc_copies, [node()]} 
     ]) of
-        {atomic, ok} -> ok;
-        {aborted, {already_exists, editor_docs}} -> ok; %% Table already exists
-        {aborted, Error} -> error(Error)
+        {atomic, ok} -> 
+            ok; %% Created fresh on this node
+        {aborted, {already_exists, editor_docs}} -> 
+            %% Table exists (e.g., on another node). 
+            %% Request a local disk replica so we persist it here too.
+            case mnesia:add_table_copy(editor_docs, node(), disc_copies) of
+                {atomic, ok} -> ok;
+                {aborted, {already_exists, editor_docs, _Node}} -> ok; %% We already have a copy
+                {aborted, Error} -> error(Error)
+            end;
+        {aborted, Error} -> 
+            error(Error)
     end,
     
-    %% 3. Wait for tables to be accessible
+    %% 3. Wait for tables (infinity is safer for clustering startup)
     mnesia:wait_for_tables([editor_docs], infinity).
